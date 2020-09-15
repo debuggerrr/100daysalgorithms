95336a1fdc9cd7c19909bd9583d4e2b7c3805e02
debuggerrr <siddhesh.kalgaonkar@ves.ac.in>
2020-09-15 20:13:41 +0530

    Add files via upload
    Binary Tree Pruning Algorithm added using Python

---
+++ BinaryTreePruning.py
@@ -0,0 +1,5 @@
+class Solution:
+    def pruneTree(self, n):
+        if n:
+            a,b = n.left,n.right = self.pruneTree(n.left), self.pruneTree(n.right)
+            return n if any([a,b,n.val]) else None
